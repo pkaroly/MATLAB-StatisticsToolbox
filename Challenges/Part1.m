cf62733d8b7480ba0bcbace6934ac70bf32f1103
Philippa Karoly <pkaroly@student.unimelb.edu.au>
2016-04-20 17:18:28 +1000

    m files

---
+++ Challenges/Part1.m
@@ -0,0 +1,56 @@
+%% Part 1 Getting the Data
+
+% The first difficulty of real data is that it comes from many different
+% sources, which may not be optimized for MATLAB. This part will cover some
+% basic tricks for automatically reading in large amounts of data
+
+%% Folder Structures
+
+% Do you have too much data to spend hours double clicking different files
+% and uploading them manually? MATLAB can automatically read in data from
+% lots of different folders on your computer, even if they have really
+% confusing names
+
+%% CHALLENGE ONE
+% Use a loop to read in the first file in each folder
+
+% Extension: use two loops to read in all the files in all the folders
+
+%% File Names
+
+% If your looking for particular file names the functions strcmp and strcmp
+% can be useful!
+
+%% CHALLENGE TWO
+
+% Work out the difference between strcmp and strfind
+
+% If you need to get particular dates, numbers or names from a file name
+% the function regexp might help.
+
+%% CHALLENGE THREE
+% Can you use regexp to find:
+
+% 1)
+
+% 2)
+
+% 3)
+
+%% CHALLENGE
+% loop through the files and read in the four excel datasheets
+% save each dataset as a matlab file called HousingPrices_01,
+% HousingPrices_02, etc in chronological order (ie you will need
+% to read and use the date from the file name)
+% HINT: the function datenum is very useful for sorting dates into
+% chronological order
+
+%% Converting Data
+
+
+%% CHALLENGE
+% the variables in HousingPrices need to be converted to something we can
+% work with
+% the column called 'Sold at Auction' contains variations of yes and no and
+% n/a. It is sometimes blank (which is the same as n/a). Convert this
+% variable into 0 = no, 1 = yes and -1 = n/a.
---
+++ Challenges/Part2.m
@@ -0,0 +1,56 @@
+%% Part 2 Visualizing Data
+
+%% Open data is everywhere!
+
+% Go to https://data.melbourne.vic.gov.au/ and see how many data sources
+% they have data on all aspects of Melbourne life! (seriously! look no further if you need
+% to know the locations of all the public toilets, or the gradient of every
+% footpath....)
+
+%% Example: Data of pedestrian numbers in the CBD
+
+% DATE_TIME
+% Contains the date and time in MATLAB's date format
+
+% HOIURLY_COUNTS
+% Contains the number of pedestrians passing in that hour
+
+% SENSOR_ID
+% Contains a number that refers to a particular location around Melbourne
+% ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+% 1 Bourke Street Mall (North)
+% 2 Bourke Street Mall (South)
+% 4 Town Hall (West)
+% 5 Princes Bridge
+% 6 Flinders Street Station Underpass
+% 9 Southern Cross Station
+% 10 Victoria Point
+% 11 Waterfront City
+% 12 New Quay
+% 13 Flagstaff Station
+% 14 Sandridge Bridge
+% 15 State Library
+% 16 Australia on Collins
+% 17 Collins Place (South)
+% 18 Collins Place (North)
+
+
+% Two tools that are used again and again when exploring data are scatter
+% plots and histograms
+
+% scatter
+
+% hist
+
+% NOTE both of these types of plots can also be achieved using the plot
+% function
+
+%% CHALLENGE
+
+% 
+
+%% CHALLENGE
+
+% Try download some other data you are interested in, read it in and 
+% visualize some aspect. You can use the user interface to try a few plots out, 
+% but once you are happy with your plot write it in a script!
